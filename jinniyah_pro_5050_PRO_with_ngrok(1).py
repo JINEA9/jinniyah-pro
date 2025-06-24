8472b4d5fd557a517b79a1320ab48ae3304bf035
JINEA9 <abana4166@gmail.com>
2025-06-24 22:07:53 +0300

    Add files via upload

---
+++ jinniyah_pro_5050_PRO_with_ngrok(1).py
@@ -0,0 +1,7 @@
+
+import os
+
+print("🚀 تشغيل JINNIYAH PRO...")
+os.system("python3 /data/data/com.termux/files/home/jinniyah/jinniyah_pro_5050.py &")
+print("🧠 تشغيل VULN_HUNTER...")
+os.system("python3 /data/data/com.termux/files/home/jinniyah/vuln_hunter_pro.py &")
